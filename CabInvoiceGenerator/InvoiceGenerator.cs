9c19a132c20543a0b362fe7a6b2aa23f3b621de1
Biru4015 <rambirendra12@gmail.com>
2020-05-11 20:12:55 +0530

    [Birendra]Refactor refactored. the code for calculating total fare for multiple rides

--- CabInvoiceGenerator/InvoiceGenerator.cs
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -8,21 +8,17 @@ namespace CabInvoiceGenerator
     public class InvoiceGenerator
     {
         // Declared and Initialised Variables
-        readonly private double distance;
-        readonly private double time;
         readonly private double COST_PER_KILO_METER = 10.0;
         readonly private double COST_PER_MININUTES = 1.0;
         readonly private double MINIMUM_FARE = 5.0;
 
         /// <InvoiceGenerator>
-        /// Parameterised Constructor
+        /// Default Constructor
         /// </InvoiceGenerator>
         /// <param name="runningDistance"></param>
         /// <param name="runningTime"></param>
-        public InvoiceGenerator(double runningDistance, double runningTime)
+        public InvoiceGenerator()
         {
-            this.distance = runningDistance;
-            this.time = runningTime;
         }
 
         /// <CalculateCabFare>
@@ -30,9 +26,28 @@ namespace CabInvoiceGenerator
         /// </CalculateCabFare>
         /// <minimumFare></returns>
         /// <totalFare></returns>
-        public double CalculateCabFare()
+        public double CalculateCabFare(double runningDistance, double runningTime)
         {
-            double totalFare = (distance * COST_PER_KILO_METER) + (time * COST_PER_MININUTES);
+            double totalFare = (runningDistance * COST_PER_KILO_METER) + (runningTime * COST_PER_MININUTES);
+            if (totalFare < MINIMUM_FARE)
+            {
+                return MINIMUM_FARE;
+            }
+            return totalFare;
+        }
+
+        /// <CalculateCabFare>
+        /// Method to calculated fare of multiple rides
+        /// </CalculateCabFare>
+        /// <param name="rides"></param>
+        /// <returns></returns>
+        public double CalculateCabFare(Ride[] rides)
+        {
+            double totalFare = 0;
+            foreach (Ride ride in rides)
+            {
+                totalFare += CalculateCabFare(ride.rideDistance, ride.rideTime);
+            }
             if (totalFare < MINIMUM_FARE)
             {
                 return MINIMUM_FARE;
---
+++ CabInvoiceGenerator/Ride.cs
@@ -0,0 +1,14 @@
+﻿namespace CabInvoiceGenerator
+{
+    public class Ride
+    {
+        // Variable
+        readonly public double rideDistance;
+        readonly public double rideTime;
+        public Ride(double runningDistance, double runningTime)
+        {
+            this.rideDistance = runningDistance;
+            this.rideTime = runningTime;
+        }
+    }
+}
