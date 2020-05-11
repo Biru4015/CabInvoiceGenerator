682c6c1f804bd7fa1f90546b02d536202510b0d2
Biru4015 <rambirendra12@gmail.com>
2020-05-11 20:36:29 +0530

    [Birendra]Add added. Test case for calculating inhanced invoice for multiple rides

--- CabInvoiceGeneratorTest/CabInvoiceTest.cs
+++ CabInvoiceGeneratorTest/CabInvoiceTest.cs
@@ -1,11 +1,14 @@
 using NUnit.Framework;
 using CabInvoiceGeneratorTest;
 using CabInvoiceGenerator;
+using System;
 
 namespace CabInvoiceGeneratorTest
 {
     public class Tests
     {
+        InvoiceGenerator invoice = new InvoiceGenerator();
+
         [SetUp]
         public void Setup()
         {
@@ -21,7 +24,6 @@ namespace CabInvoiceGeneratorTest
         {
             double cabRunningDistance = 5.0;
             double cabRunningTime = 2.0;
-            InvoiceGenerator invoice = new InvoiceGenerator();
             Assert.AreEqual(52, invoice.CalculateCabFare(cabRunningDistance, cabRunningTime));
         }
 
@@ -35,24 +37,59 @@ namespace CabInvoiceGeneratorTest
         {
             double cabRunningDistance = 0.1;
             double cabRunningTime = 1.0;
-            InvoiceGenerator invoice = new InvoiceGenerator();
             Assert.AreEqual(5, invoice.CalculateCabFare(cabRunningDistance, cabRunningTime));
         }
 
-        /// <test 2>
-        /// Creating an object of InvoiceGenerator
+        /// <summary>
+        /// Test case 2.1
         /// sending two parameters as cabRunningDistance and canRunningTime
-        /// </test 2>
+        /// </summary>
         [Test]
         public void GivenDistanceAndTimeOfMultiRidesToInvoiceGenerator_WhenCalculate_ShouldReturnTotalFare()
         {
+            /// Multiple ride array
             Ride[] rides =
                 {
                 new Ride(2.0,1.0),
                 new Ride(2.5,1.5)
                 };
-            InvoiceGenerator invoice = new InvoiceGenerator();
-            Assert.AreEqual(47.5, invoice.CalculateCabFare(rides));
+            var exceptedSummery = 47.5;
+            InvoiceSummary returnSummery = invoice.CalculateCabFare(rides);
+            Assert.AreEqual(exceptedSummery, returnSummery.totalFare);
+        }
+
+        /// <summary>
+        /// Test case 3.1
+        /// Creating an object of InvoiceGenerator
+        /// sending two parameters as cabRunningDistance and canRunningTime
+        /// Total Number of Rides- Total Fare- Average Fare Per Ride
+        /// </summary>
+        [Test]
+        public void GivenDistanceAndTimeOfMultiRidesToInvoiceGenerator_WhenCalculated_ShouldInhancedInvoice()
+        {
+            //// Local variables
+            bool exceptedInvoice = true;
+            bool returnInvoice = false;
+            //// sending two rides distance in double and also time in double
+            Ride[] rides =
+            {
+                new Ride(2.0,1.0),
+                new Ride(2.5,1.5)
+            };
+            InvoiceSummary returnSummery = invoice.CalculateCabFare(rides);
+            InvoiceSummary expectedSummery = new InvoiceSummary
+            {
+                totalNumberOfRides = 2,
+                totalFare = 47.5,
+                averageFarePerRide = 23.75
+            };
+            //// Checkoing all three returnSummary values are equal to exceptedSummary
+            //// IF yes then returnInvoice will be 'true'
+            if (returnSummery.totalNumberOfRides == expectedSummery.totalNumberOfRides && returnSummery.totalFare == expectedSummery.totalFare && returnSummery.averageFarePerRide == expectedSummery.averageFarePerRide)
+            {
+                returnInvoice = true;
+            }
+            Assert.AreEqual(exceptedInvoice, returnInvoice);
         }
     }
 }
