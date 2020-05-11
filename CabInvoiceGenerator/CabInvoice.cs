4272d1635df8dabbbadb9df89317942e74287907
Biru4015 <rambirendra12@gmail.com>
2020-05-11 18:11:14 +0530

    [Birendra]Add added. welcome message

---
+++ CabInvoiceGenerator/CabInvoice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    class CabInvoice
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO CAB INVOICE GENERATOR PROJECT!!");
+        }
+    }
+}
