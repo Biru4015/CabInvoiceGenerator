76018e833697fc749105c50802068146fe36bde4
Biru4015 <rambirendra12@gmail.com>
2020-05-11 20:15:11 +0530

    [Birendra]Add added. Test case for calculating fare multiple rides

--- CabInvoiceGeneratorTest/CabInvoiceTest.cs
+++ CabInvoiceGeneratorTest/CabInvoiceTest.cs
@@ -21,8 +21,8 @@ namespace CabInvoiceGeneratorTest
         {
             double cabRunningDistance = 5.0;
             double cabRunningTime = 2.0;
-            InvoiceGenerator invoice = new InvoiceGenerator(cabRunningDistance, cabRunningTime);
-            Assert.AreEqual(52, invoice.CalculateCabFare());
+            InvoiceGenerator invoice = new InvoiceGenerator();
+            Assert.AreEqual(52, invoice.CalculateCabFare(cabRunningDistance, cabRunningTime));
         }
 
         /// <summary>
@@ -35,8 +35,24 @@ namespace CabInvoiceGeneratorTest
         {
             double cabRunningDistance = 0.1;
             double cabRunningTime = 1.0;
-            InvoiceGenerator invoice = new InvoiceGenerator(cabRunningDistance, cabRunningTime);
-            Assert.AreEqual(5, invoice.CalculateCabFare());
+            InvoiceGenerator invoice = new InvoiceGenerator();
+            Assert.AreEqual(5, invoice.CalculateCabFare(cabRunningDistance, cabRunningTime));
+        }
+
+        /// <test 2>
+        /// Creating an object of InvoiceGenerator
+        /// sending two parameters as cabRunningDistance and canRunningTime
+        /// </test 2>
+        [Test]
+        public void GivenDistanceAndTimeOfMultiRidesToInvoiceGenerator_WhenCalculate_ShouldReturnTotalFare()
+        {
+            Ride[] rides =
+                {
+                new Ride(2.0,1.0),
+                new Ride(2.5,1.5)
+                };
+            InvoiceGenerator invoice = new InvoiceGenerator();
+            Assert.AreEqual(47.5, invoice.CalculateCabFare(rides));
         }
     }
 }
